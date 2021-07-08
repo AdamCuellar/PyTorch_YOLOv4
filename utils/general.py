078eb76c4fe46abbc108a6275106dcc8813c3464
AdamCuellar <cuellar.adam@gmail.com>
2021-07-07 22:00:46 -0400

    Add max_det as argument

--- utils/general.py
+++ utils/general.py
@@ -148,7 +148,7 @@ def build_targets(p, targets, model):
     return tcls, tbox, indices, anch
 
 def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.6, merge=False, classes=None,
-                        agnostic=False, nmsType="normal", beta1=0.6):
+                        agnostic=False, nmsType="normal", beta1=0.6, max_det=300):
     """Performs Non-Maximum Suppression (NMS) on inference results
 
     Returns:
@@ -162,7 +162,7 @@ def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.6, merge=False, 
 
     # Settings
     min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
-    max_det = 300  # maximum number of detections per image
+    # max_det = 300  # maximum number of detections per image
     time_limit = 10.0  # seconds to quit after
     redundant = True  # require redundant detections
     multi_label = nc > 1  # multiple labels per box (adds 0.5ms/img)
